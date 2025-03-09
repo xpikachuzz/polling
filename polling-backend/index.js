7761919cdbf72c266ad6501051a2aaec73d1ec4e
rbas0013 <rbas0013@student.monash.edu>
2025-03-10 01:41:20 +1100

    smal

--- polling-backend/index.js
+++ polling-backend/index.js
@@ -1,46 +1,47 @@
 const express = require("express");
 const pollRouter = require("./routes/poll");
-const app = express();    // initiate express.
+const app = express(); // initiate express.
 const cors = require("cors");
 
 // redis
-const redisClient = require("./redis")
+const redisClient = require("./redis");
 const { Server } = require("socket.io");
-const { join_room, leave_room, vote_room, initializeUser } = require("./socket/socketControllers");
+const {
+    join_room,
+    leave_room,
+    vote_room,
+    initializeUser,
+} = require("./socket/socketControllers");
 const authRouter = require("./routes/authRouter");
 const { sessionMiddleWare, wrap } = require("./controllers/serverController");
-
 
 const server = require("http").createServer(app);
 
 const io = new Server(server, {
-    cors: { origin:"http://localhost:5173", credentials: true },
+    cors: { origin: "http://localhost:5173", credentials: true },
 });
 
-redisClient.connect().catch(console.error)
-
-
+redisClient.connect().catch(console.error);
 
 app.use(express.json());
-app.use(sessionMiddleWare)
-app.use(cors({credentials: true, origin: "http://localhost:5173"}));
+app.use(sessionMiddleWare);
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
 // if a GET request comes through the '/' path then pass the request throught the following chain of middleware functions.
 app.use("/poll", pollRouter);
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 
-
-
-io.use(wrap(sessionMiddleWare))
+io.use(wrap(sessionMiddleWare));
 
 io.on("connect", (socket) => {
-  initializeUser(socket)
-  // Join room open emission
-  socket.on("poll_room_join", (id) => join_room(id, socket));
-  socket.on("poll_room_leave", (id) => leave_room(id, socket));
-  socket.on("poll_room_vote", (vote_data) => vote_room(vote_data, socket, io));
-})
-
+    initializeUser(socket);
+    // Join room open emission
+    socket.on("poll_room_join", (id) => join_room(id, socket));
+    socket.on("poll_room_leave", (id) => leave_room(id, socket));
+    socket.on("poll_room_vote", (vote_data) =>
+        vote_room(vote_data, socket, io)
+    );
+});
 
 // io.of("/").adapter.on("create-room", (room) => {
 //   console.log(`room ${room} was created`);
@@ -54,11 +55,11 @@ io.on("connect", (socket) => {
 //   console.log(`socket ${id} has left room ${room}`);
 // });
 
-
-
 const PORT = process.env.PORT || 3000;
 
 // Server will listen for incoming requests
 server.listen(PORT, () => {
-  console.log(`My first Express app - listening on port http://localhost:${PORT}/ !`);
+    console.log(
+        `My first Express app - listening on port http://localhost:${PORT}/ !`
+    );
 });
--- polling-backend/routes/authRouter.js
+++ polling-backend/routes/authRouter.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const {validateController, loginPost, loginGet, signupPost} = require("../controllers/authControllers");
-
-
+const {
+    validateController,
+    loginPost,
+    loginGet,
+    signupPost,
+} = require("../controllers/authControllers");
 
 const authRouter = express();
 
 authRouter.post("/login", validateController, loginPost);
-authRouter.get("/login", loginGet)
-
+authRouter.get("/login", loginGet);
 
 authRouter.post("/register", validateController, signupPost);
 
